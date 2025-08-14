614f4f9280d5848154825be165afb6c21bba2705
Aremju <juliusarendt@icloud.com>
2025-08-14 23:03:57 +0200

    #2: add next rules to eslint

--- apps/demo-web/eslint.config.mjs
+++ apps/demo-web/eslint.config.mjs
@@ -1,12 +1,21 @@
 import js from '@eslint/js';
 import tseslint from 'typescript-eslint';
 import globals from 'globals';
+import { FlatCompat } from '@eslint/eslintrc';
+
+const compat = new FlatCompat({
+  baseDirectory: import.meta.dirname,
+});
 
 export default [
   { ignores: ['**/node_modules/**', '**/dist/**', '**/.next/**', '**/.turbo/**'] },
 
   js.configs.recommended,
   ...tseslint.configs.recommended,
+
+  ...compat.config({
+    extends: ['next/core-web-vitals'],
+  }),
 
   {
     files: ['**/*.{ts,tsx,js,jsx}'],
